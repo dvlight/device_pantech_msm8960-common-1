612ae83dcca93c5ee6fecf69a5e0c2d29d9c1889
light1234 <45263506+Dev-light1234@users.noreply.github.com>
2019-09-19 16:16:43 +0900

    ef52l: include base_rules.mk

--- Android.mk
+++ Android.mk
@@ -4,6 +4,8 @@ ifneq ($(filter ef52 ef52l ef52s ef52k a870 a870l a870s a870k,$(TARGET_DEVICE)),
 
 $(warning $(TARGET_DEVICE) device found for iron-common)
 
+include $(BUILD_SYSTEM)/base_rules.mk
+
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
 include $(CLEAR_VARS)

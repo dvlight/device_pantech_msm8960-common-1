f5aa49c35d3a8fbbe2f87d0a396516153c9f483c
light1234 <45263506+Dev-light1234@users.noreply.github.com>
2019-09-19 16:15:34 +0900

    ef52l: add Android.mk

---
+++ Android.mk
@@ -0,0 +1,11 @@
+LOCAL_PATH := $(call my-dir)
+
+ifneq ($(filter ef52 ef52l ef52s ef52k a870 a870l a870s a870k,$(TARGET_DEVICE)),)
+
+$(warning $(TARGET_DEVICE) device found for iron-common)
+
+include $(call all-makefiles-under,$(LOCAL_PATH))
+
+include $(CLEAR_VARS)
+
+endif

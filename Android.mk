48f74ba1697339fed392425e612b93a3ada46c3e
light1234 <45263506+Dev-light1234@users.noreply.github.com>
2019-09-20 09:11:40 +0900

    wifi_symlinks

--- Android.mk
+++ Android.mk
@@ -4,10 +4,15 @@ ifneq ($(filter ef52 ef52l ef52s ef52k a870 a870l a870s a870k,$(TARGET_DEVICE)),
 
 $(warning $(TARGET_DEVICE) device found for iron-common)
 
-include $(BUILD_SYSTEM)/base_rules.mk
-
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
 include $(CLEAR_VARS)
+
+LOCAL_MODULE := wifi_symlinks
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := FAKE
+LOCAL_MODULE_SUFFIX := -timestamp
+
+include $(BUILD_SYSTEM)/base_rules.mk
 
 endif
